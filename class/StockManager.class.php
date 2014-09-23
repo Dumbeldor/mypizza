616cd29b30c026ab6c6aaa56e2c992ea549af32a
Dumbeldor <vincent.glize@live.fr>
2014-09-23 20:04:53 +0200

    StockManager.php

--- class/StockManager.class.php
+++ class/StockManager.class.php
@@ -1,5 +1,5 @@
 <?php
-class UserManager
+class StockManager
 {
 	private $_db; //Instance PDO
 
@@ -17,78 +17,48 @@ class UserManager
 		$q->binValue(':idPizzeria', $stock->idPizzeria());
 		$q->binValue(':idProduit', $stock->idProduit());
 	}
-	//Supprimer un user dans la BDD
+	//Supprimer un stock dans la BDD
 	public function delete(Stock $stock)
 	{
 		$this->_db->exec('DELETE FROM stock WHERE id = '.$stock->id());
 	}
-	//Récupérer un user dans la BDD pour le stocker dans une nouvelle instance user
+	//Récupérer un stock dans la BDD pour le stocker dans une nouvelle instance stock
 	public function get($info)
 	{
 		if(is_int($info))
 		{
-			$q = $this->_db->query('SELECT id, nom, prenom, pseudo, pass, email FROM user WHERE id = '.$info);
+			$q = $this->_db->query('SELECT id, idPizzeria, idProduit FROM stock WHERE id = '.$info);
 			while($donnees = $q->fetch())
 			{
-				return new User(array(
+				return new Stock(array(
 					'id' => $donnees['id'],
-					'nom' => $donnees['nom'],
-					'prenom' => $donnees['prenom'],
-					'pseudo' => $donnees['pseudo'],
-					'pass' => $donnees['pass'],
-					'email' => $donnees['email'],
+					'idPizzeria' => $donnees['idPizzeria'],
+					'idProduit' => $donnees['idProduit'],
 					));
 			}
 		}
 		else {
-			$q = $this->_db->query('SELECT id, nom, prenom, pseudo, pass, email FROM user WHERE pseudo = '.$info);
-			while($donnees = $q->fetch())
-			{
-				return new User(array(
-					'id' => $donnees['id'],
-					'nom' => $donnees['nom'],
-					'prenom' => $donnees['prenom'],
-					'pseudo' => $donnees['pseudo'],
-					'pass' => $donnees['pass'],
-					'email' => $donnees['email'],
-					));
-			}
+			echo "Erreur, vous devez mettre une valeur numerique !";
 		}			
 	}
-	//Voir si le pseudo existe déjà
+	//Voir si le stock existe déjà
 	public function exists($info)
 	{
 		//Vérification avec ID
 		if(is_int($info)){
-			return (bool) $this->_db->query('SELECT COUNT(*) FROM user WHERE id = '.$info)->fetchColumn();
+			return (bool) $this->_db->query('SELECT COUNT(*) FROM stock WHERE id = '.$info)->fetchColumn();
 		}
-		$q = $this->_db->prepare('SELECT COUNT(*) FROM user WHERE pseudo = :pseudo');
-		$q->execute(array(':pseudo' => $info));
-		return (bool) $q->fetchColumn();
 	}
 	//mettre à jours
-	public function update(User $user)
+	public function update(Stock $stock)
 	{
-		$id = $user->id();
+		$id = $stock->id();
 		if(isset($id))
 		{
-			$q = $this->_db->prepare('UPDATE personnage SET nom = :nom, prenom = :prenom, pseudo = :pseudo, pass = :pass, email = :email WHERE id = :id');
-			$q->bindValue('id', $user->id());
-			$q->bindValue('nom', $user->nom());
-			$q->bindValue('prenom', $user->prenom());
-			$q->bindValue('pseudo', $user->pseudo());
-			$q->bindValue('pass', $user->pass());
-			$q->bindValue('email', $user->email());	
-			$q->execute();
-		}
-		else { //Si nous voulons updat avec un nom d'utilisateurs
-			$q = $this->_db->prepare('UPDATE personnage SET nom = :nom, prenom = :prenom, pass = :pass, email = :email WHERE pseudo = :pseudo');
-			$q->bindValue('id', $user->id());
-			$q->bindValue('nom', $user->nom());
-			$q->bindValue('prenom', $user->prenom());
-			$q->bindValue('pseudo', $user->pseudo());
-			$q->bindValue('pass', $user->pass());
-			$q->bindValue('email', $user->email());	
+			$q = $this->_db->prepare('UPDATE stock SET idPizzeria = :idPizzeria, idProduit = :idProduit WHERE id = :id');
+			$q->bindValue('id', $stock->id());
+			$q->bindValue('idPizzeria', $stock->idPizzeria());
+			$q->bindValue('idProduit', $stock->idProduit());
 			$q->execute();
 		}
 	}
