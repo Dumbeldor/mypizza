a4718d0df2d0a0c788b0298341f23cd9b6799653
scra <alban.bertoliniweb@laposte.net>
2014-10-05 17:06:13 +0200

    Formulaire user manager Problème mdp

--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <form action="include/connexion.php" method="post">
 		  <p>
-			Nom de votre Pizzeria :<br /><input type="text" name="nomPizzeria" maxlength="50" class="inputConn"><br /><br />
+			Nom de votre Pizzeria :<br /><input type="text" name="pseudo" maxlength="50" class="inputConn"><br /><br />
 			mot de passe :<br /><input type="password" name="pass" maxlength="50" class="inputConn">
-			<input type="submit" value="Créer ce personnage" name="creer" class="submit">
+			<input type="submit" value="Se connecter" name="creer" class="submit">
 		  </p>
 		</form>
